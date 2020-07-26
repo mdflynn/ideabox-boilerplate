da094afa04fae62829c56a4bc7e1d3eccfd6ee8b
Estelle Staffieri <estellelareinne@gmail.com>
2020-07-25 20:29:54 -0600

    Connecting to the input fields and creating a new idea card.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 var saveButton = document.querySelector(".save-button");
-var titleInput = document.querySelector("#title-text");
-var bodyInput = document.querySelector("#body-text");
+var ideaCardText = document.querySelector(".idea-title");
+var ideaBodyText = document.querySelector(".idea-text");
 
+idea = new Idea(title, body)
 
 saveButton.addEventListener("click", addInput);
 
 function addInput() {
-
+  var titleInput = document.querySelector("#title-text").value;
+  var bodyInput = document.querySelector("#body-text").value;
+  titleInput.innerText = ideaCardText;
+  bodyInput.innerText = ideaBodyText;
 }
