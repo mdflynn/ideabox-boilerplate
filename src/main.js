41906343066114338c21c3c50af5f02a43e8df7f
Estelle Staffieri <estellelareinne@gmail.com>
2020-07-25 20:17:42 -0600

    Add querySelectors for inital user journey and build out first function

---
+++ src/main.js
@@ -0,0 +1,10 @@
+var saveButton = document.querySelector(".save-button");
+var titleInput = document.querySelector("#title-text");
+var bodyInput = document.querySelector("#body-text");
+
+
+saveButton.addEventListener("click", addInput);
+
+function addInput() {
+
+}
