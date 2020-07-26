5dfb24e8a63014a75c3500f6a9edfc9e9913a176
Estelle Staffieri <estellelareinne@gmail.com>
2020-07-26 12:43:18 -0600

    Start interpolating the new saved idea to the DOM

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ var saveButton = document.querySelector(".save-button");
 var ideaCardText = document.querySelector(".idea-title");
 var ideaBodyText = document.querySelector(".idea-text");
 var favoriteIdea = document.querySelector(".favorite-image-unsaved");
+var titleInput = document.querySelector("#title-text");
+var bodyInput = document.querySelector("#body-text");
 
 // var savedIdeas = [];
 
@@ -11,13 +13,27 @@ saveButton.addEventListener("click", addInput);
 favoriteIdea.addEventListener("click",saveIdea);
 
 function addInput() {
-  var titleInput = document.querySelector("#title-text").value;
-  var bodyInput = document.querySelector("#body-text").value;
+  titleInput.value;
+  bodyInput.value;
   titleInput.innerText = ideaCardText;
   bodyInput.innerText = ideaBodyText;
 }
 
 function saveIdea() {
-  document.getElementsByClassName(".favorite-image-unsaved").src="./assets/star-active.svg";
+  var newIdea = `<article class="one-idea-card">
+    <div class="idea-card-header">
+      <img class="favorite-image-unsaved" src="./assets/star-active.svg" height="25" width="25">
+      <img class="delete-image-unsaved" src="./assets/delete.svg" height="25" width="25">
+    </div>
+    <div class="idea-card-body">
+      <p class="idea-title">${titleInput.value}</p>
+      <p class="idea-text">${bodyInput.value}</p>
+    </div>
+    <div class="idea-card-footer">
+      <img class="comment-image" src="./assets/comment.svg" height="25" width="25">
+      <p class="idea-comments">Comment</p>
+    </div>
+  </article>`
+  ideaGrid.innerHTML += newIdea
   // savedIdeas.push()
 }
